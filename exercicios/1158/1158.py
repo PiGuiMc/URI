fb3295cafb044b5e3d19e30d528e110360265793
Jose Costa <jose.costa@envolti.com.br>
2021-11-09 19:38:49 -0300

    commit 1158

--- exercicios/1158/1158.py
+++ exercicios/1158/1158.py
@@ -1,8 +1,21 @@
 n = int(input())
-i = 1
 
-while n>= 0:
-    if n >= 0:
-        print(i)
-        i +=2
-        n = n -1
+for i in range(0,n):
+    a = input().split()
+    x,y = a
+    x = int(x)
+    y = int(y)
+    soma = 0
+    '''controle'''
+    j = 1
+
+    while j <= y:
+        if x % 2 != 0:
+
+            soma = soma + x
+
+            x = x + 1
+            j = j + 1
+        if x % 2 == 0:
+            x = x + 1
+    print(soma)
