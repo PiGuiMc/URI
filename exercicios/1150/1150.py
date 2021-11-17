f011f87e768d580767b54bf565b95bc9ee49c716
Jose Costa <jose.costa@envolti.com.br>
2021-11-17 17:37:03 -0300

    new change

--- exercicios/1150/1150.py
+++ exercicios/1150/1150.py
@@ -1,14 +1,14 @@
 x = int(input())
 z = x - 1
 i = 2
-s = 1
+S = 1
 soma = x
 while z <= x:
     z = int(input())
 while soma <= z:
-    soma = soma + x + s
+    soma = soma + x + S
     
     if soma <= z:
         i = i + 1
-        s = s + 1
+        S = S + 1
 print(i)
