c6b60cb16fb901ccfed9251a47c05a1b91fe8023
Jose Costa <jose.costa@envolti.com.br>
2022-01-02 12:02:39 -0300

    New Commit

---
+++ exercicios/C#/1038_c#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1038_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Valor = {' ', 4, 4.50, 5, 2, 1.5};
+
+            String Line = Console.ReadLine();
+            String[] Valores = Line.Split(' ');
+
+            int Codigo = Convert.ToInt32(Valores[0]);
+            int Qt = Convert.ToInt32(Valores[1]);
+            
+
+            double Total = Convert.ToDouble(Qt) * Valor[Codigo];
+            Console.WriteLine("Total: R$ {0}",Total.ToString("0.00"));
+
+
+        }
+    }
+}
