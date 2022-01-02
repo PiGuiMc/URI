cd3f9c5414a6e6fd1e745e933be32910c3e43024
Jose Costa <jose.costa@envolti.com.br>
2022-01-02 12:02:53 -0300

    New COmmit

---
+++ exercicios/C#/1035_c#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1035_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Line = Console.ReadLine();
+
+            String[] Valores = Line.Split(' ');
+            
+            int A = Convert.ToInt32(Valores[0]);
+            int B = Convert.ToInt32(Valores[1]);
+            int C = Convert.ToInt32(Valores[2]);
+            int D = Convert.ToInt32(Valores[3]);
+
+            if(B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+
+        }
+    }
+}
---
+++ exercicios/C#/1036_c#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1036_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Line = Console.ReadLine();
+
+            String[] Valores = Line.Split(' ');
+
+            double A = Convert.ToDouble(Valores[0]);
+            double B = Convert.ToDouble(Valores[1]);
+            double C = Convert.ToDouble(Valores[2]);
+
+            double Delta = B*B - 4*A*C;
+
+            double Raiz = Math.Sqrt(Delta);
+
+            double Div = (2 * A);
+
+            double R1 = (-B +  Raiz) / Div;
+
+            double R2 = (-B - Raiz) / Div;
+
+            if((Div == 0) || (Delta < 0))
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                Console.WriteLine("R1 = {0}",R1.ToString("0.00000"));
+                Console.WriteLine("R2 = {0}",R2.ToString("0.00000"));
+            }
+
+            
+        }
+    }
+}
