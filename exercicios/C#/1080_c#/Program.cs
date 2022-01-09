4dfdfaba03767669db9dd6a94df48439745539ad
Jose Costa <jose.costa@envolti.com.br>
2022-01-09 16:12:24 -0300

    new commit

--- exercicios/C#/1080_c#/Program.cs
+++ exercicios/C#/1080_c#/Program.cs
@@ -7,21 +7,19 @@ namespace _1080_c_
         static void Main(string[] args)
         {
             int i, pos = 0, maior;
-            
-
-            int []n = Convert.ToInt32(Console.ReadLine(n));
-            maior = n;
-    
-            for (i = 1; i < 100; i++){
-                Console.ReadLine(n);
         
-                if (n[i] > maior) {
-                    maior = n[i];
+            maior = 0;
+    
+            for (i = 1; i < 101; i++){
+                int v = Convert.ToInt32(Console.ReadLine());
+        
+                if (v > maior) {
+                    maior = v;
                     pos = i;
                 }
             }
             Console.WriteLine(maior);
-            Console.WriteLine((pos + 1).ToString());
+            Console.WriteLine((pos).ToString());
         }
     }
 }
