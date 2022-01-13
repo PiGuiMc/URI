90b2b4a4587f31a322a8d09850518cdc09420281
Jose Costa <jose.costa@envolti.com.br>
2022-01-13 10:33:49 -0300

    new commit

---
+++ exercicios/C#/1145_c#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11145_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] vet = Console.ReadLine().Split(' ') ;
+        int x,y,count;
+        x = int.Parse(vet[0]);
+        y = int.Parse(vet[1]);
+        count = 1;
+
+        for (int i = 1;i <= y;i++)
+        {
+            Console.Write(i);
+            if (x == count)
+            {
+                Console.WriteLine();
+                count = 1;
+            }
+            else if(count < x)
+            {
+                Console.Write(" ");
+                count++;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ exercicios/C#/1146_c#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1146_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n;
+           int[] dados = new int[n];
+
+           while(true){
+            
+               n = int.Parse(Console.ReadLine());
+
+               if(n == 0){
+                   break;
+               }
+
+               for(int i =1;i<=n;i++){
+                   Add(dados[i] = i);
+               }
+               Console.WriteLine(dados);
+           }
+        }
+    }
+}
