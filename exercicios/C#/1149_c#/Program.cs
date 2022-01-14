4b9c940763d3c54c874883af2d5f8d64d37fa44a
Jose Costa <jose.costa@envolti.com.br>
2022-01-14 07:35:14 -0300

    new commit

---
+++ exercicios/C#/1149_c#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _119_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        
+        int a, n;
+
+        string[] vet = Console.ReadLine().Split(' ');
+        a = int.Parse(vet[0]);
+        n = int.Parse(vet[1]);
+
+        int contv = 1;
+        while (n <= 0)
+        {
+            contv++;
+            n = int.Parse(vet[contv]);
+
+        }
+        int soma = 0;
+        for (int i = 0; i <= n - 1; i++)
+        {
+            soma = soma + (a + i);
+
+        }
+        Console.WriteLine(soma);
+        }
+    }
+}
