d1629d13bc058cae0bb6d37eae2557d166980dae
Jose Costa <jose.costa@envolti.com.br>
2022-01-20 16:42:31 -0300

    ne commit

--- exercicios/R/1179/1179.r
+++ exercicios/R/1179/1179.r
@@ -1,42 +1,56 @@
 input = file('stdin','r')
 
-impar <- c(15)
-par <- c(15)
+qt <- (1:15)
 
-for(i in 1:15){
+p = as.integer(1)
+im = as.integer(1)
+j = as.integer(1)
+par <- c(5)
+impar <- c(5)
+
+for(i in qt){
     num = as.integer(readLines(input,n=1))
 
     if(num%%2==0){
-        par[a] = num
-    }else if(num%%2!=0){
-        impar[a] = num
+        par[p] = num
+        p = p + 1
     }
-    if(length(par)==5){
-        ix = 0
-        for(v in par){
-            write(sprintf("par[%d] = %d", ix,v),'')
-            ix = ix + 1
-        }
+    else if(num%%2!=0){
+        impar[im] = num
+        im = im + 1
     }
-    if(length(impar)==5){
-        ix = 0
-        for(v in impar){
-            write(sprintf("impar[%d] = %d", ix,v),'')
-            ix = ix + 1
+
+     if(length(par)==5){
+        while(j<=5){
+            write(sprintf("par[%d] = %d",j-1,par[j]),'')
+            j = j + 1
         }
+        j = 0
+        p = 1
+        par <- c(0)
+    }
+    else if(length(impar)==5){
+        while(j<=5){
+            write(sprintf("impar[%d] = %d",j-1,impar[j]),'')
+            j = j + 1
+        }
+        j = 1
+        im = 1
+        impar <- c(0)
     }
 }
-if(length(impar)> 0){
-    ix = 0
-    for(v in impar){
-        write(sprintf("impar[%d] = %d", ix,v),'')
-        ix = ix + 1
+
+if(length(impar)>0){
+    while(j<=length(impar)){
+        write(sprintf("impar[%d] = %d",j-1,na.omit(impar[j])),'')
+        j = j + 1
     }
+    j = 1
 }
 if(length(par)>0){
-    ix = 0
-    for( v in par){
-        write(sprintf("par[%d] = %d",ix,v),'')
-        ix = ix + 1
+    while(j<=length(par)){
+        write(sprintf("par[%d] = %d",j-1,na.omit(par[j])),'')
+        j = j + 1
     }
+    j = 0
 }
