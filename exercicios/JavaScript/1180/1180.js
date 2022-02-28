84bad725f57bdaecaa18ea975c7a32a67bc2adb4
PiGuiMc <guimacedosota@gmail.com>
2022-02-28 14:50:22 -0300

    new commit

--- exercicios/JavaScript/1180/1180.js
+++ exercicios/JavaScript/1180/1180.js
@@ -1,17 +1,17 @@
 var input = require('fs').readFileSync('stdin','utf8');
 var lines = input.split('\n');
 
-var n =  parseInt(lines.shift()), menor=1000, pos=0;
+var n =  parseInt(lines.shift());
 var line1 = lines.shift().split(' ');
-
+var list = [n];
+var aList = [n];
 for(let i=0; i<n; i++){
     var valor = line1[i];
-    console.log(valor)
-
-    if(valor < menor){
-        menor = valor;
-        pos = i;
-    }
+    list[i] = valor;
+    aList[i] = valor;
 }
-console.log(`Menor valor: ${menor}`);
-console.log(`Posicao: ${pos}`)
+
+var nList = list.sort();
+
+console.log(`Menor valor: ${nList[0]}`);
+console.log(`Posicao: ${aList.indexOf(nList[0])}`);
